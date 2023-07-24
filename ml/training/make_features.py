bcd6efa1a943d65aba92d16bd099333b837ee7b9
zeinovich <nikita.zinovich@gmail.com>
2023-07-24 12:26:47 +0300

    styling

--- ml/training/make_features.py
+++ ml/training/make_features.py
@@ -16,12 +16,11 @@ from torchvision import transforms as T
 from utils import make_url_df
 
 
-
 class ImageURLWithoutClasses(Dataset):
     def __init__(self, url_df: pd.DataFrame, transform=None):
         self.url_df = url_df
         self.transform = transform
-        
+
     def __getitem__(self, index):
         idx, url = self.url_df.iloc[index]
         response = requests.get(url)
@@ -30,19 +29,16 @@ class ImageURLWithoutClasses(Dataset):
 
         if self.transform is not None:
             x = self.transform(x)
-            
+
         return x, y
-    
+
     def __len__(self):
         return len(self.url_df)
 
 
 @torch.no_grad()
-def get_embeddings(model,
-                   dataloader,
-                   device,
-                   batch) -> np.ndarray:
-    
+def get_embeddings(model, dataloader, device, batch) -> np.ndarray:
+
     model.eval()
     model.to(device)
     embeddings = {}
@@ -51,32 +47,32 @@ def get_embeddings(model,
 
         x = x.to(device)
         emb = model(x)
-        
+
         emb = emb.detach().cpu().numpy()
         emb = emb.reshape(batch, -1)
-        
+
         idx = idx.detach().cpu().numpy()
         idx = int(idx.reshape(1, -1)[0])
-    
+
         embeddings[idx] = emb
-        
+
     return embeddings
 
 
 def process_embeddings(embeddings: dict) -> pd.DataFrame:
     """
     Process the embeddings dictionary into a numpy array.
-    
+
     Args:
         embeddings (dict): Dictionary with the embeddings.
-        
+
     Returns:
         np.ndarray: Numpy array with the embeddings.
     """
-    
+
     output_dim = list(embeddings.values())[0].shape[1]
     embeddings = {k: v.reshape(output_dim) for k, v in embeddings.items()}
-    emb_df = pd.DataFrame.from_dict(embeddings, orient='index')
+    emb_df = pd.DataFrame.from_dict(embeddings, orient="index")
     emb_df = emb_df.reset_index()
     emb_df = emb_df.rename(columns={"index": "id"})
     emb_df = emb_df.sort_values(by="id")
@@ -95,7 +91,7 @@ def read_input_file(input_file: str) -> pd.DataFrame:
 
     else:
         raise ValueError("The input file must be a CSV or a pickle file.")
-    
+
     return df
 
 
@@ -112,11 +108,23 @@ def save_output_file(emb_df: pd.DataFrame, output_file: str):
     else:
         raise ValueError("The output file must be a CSV or a pickle file.")
 
+
 def cli():
-    parser = argparse.ArgumentParser(description="Make embeddings for the images in the dataset.")
-    parser.add_argument("--input_file", type=str, default="data", help="Path to the input file.")
-    parser.add_argument("--output_file", type=str, default="data", help="Path to the output file.")
-    parser.add_argument("--view", type=str, default="default", help="View of the image to use.")
+    parser = argparse.ArgumentParser(
+        description="Make embeddings for the images in the dataset."
+    )
+    parser.add_argument(
+        "--input_file", type=str, default="data", help="Path to the input file."
+    )
+    parser.add_argument(
+        "--output_file", type=str, default="data", help="Path to the output file."
+    )
+    parser.add_argument(
+        "--view",
+        type=str,
+        default="default",
+        help="View of the image to use. One of [default, left, right]",
+    )
     parser.add_argument("--model_path", type=str, help="Path to the model.")
     parser.add_argument("--img_size", type=int, default=224, help="Image size.")
     parser.add_argument("--batch_size", type=int, default=1, help="Batch size.")
@@ -139,40 +147,38 @@ def main():
 
     # Load the model.
     model = torch.load(args.model_path, map_location=args.device)
-    print('model loaded')
+    print("model loaded")
 
     # Make the URL DataFrame.
     df = read_input_file(INPUT_FILE)
     url_df = make_url_df(df, view=args.view)
-    print(f'url_df made {url_df.shape}')
+    print(f"url_df made {url_df.shape}")
 
     # Make the Dataset and DataLoader.
-    transform = T.Compose([
-        T.Resize((IMG_SIZE, IMG_SIZE)),
-        T.ToTensor(),
-        T.Normalize(mean=[0.485, 0.456, 0.406],
-                    std=[0.229, 0.224, 0.225]),
-    ])
+    transform = T.Compose(
+        [
+            T.Resize((IMG_SIZE, IMG_SIZE)),
+            T.ToTensor(),
+            T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
     dataset = ImageURLWithoutClasses(url_df, transform=transform)
-    dataloader = DataLoader(dataset,
-                            batch_size=BATCH_SIZE,
-                            num_workers=NUM_WORKERS,
-                            shuffle=False)
-    print('dataset and dataloader made')
+    dataloader = DataLoader(
+        dataset, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, shuffle=False
+    )
+    print("dataset and dataloader made")
 
     # Get the embeddings.
-    embeddings = get_embeddings(model,
-                                dataloader,
-                                device=args.device,
-                                batch=BATCH_SIZE)
-    print('embeddings made')
+    embeddings = get_embeddings(model, dataloader, device=args.device, batch=BATCH_SIZE)
+    print("embeddings made")
     emb_df = process_embeddings(embeddings)
-    print('embeddings processed')
+    print("embeddings processed")
 
     # Save the embeddings.
-    
+
     save_output_file(emb_df, OUTPUT_FILE)
-    print('embeddings saved')
+    print("embeddings saved")
+
 
 if __name__ == "__main__":
     main()
--- ml/training/utils.py
+++ ml/training/utils.py
@@ -1,21 +1,22 @@
 import numpy as np
 import pandas as pd
 
+
 def make_url_df(df: pd.DataFrame, view="default") -> pd.DataFrame:
     """
     Make a DataFrame with the image URLs for each ID.
-    
+
     Args:
         df (pd.DataFrame): DataFrame with the IDs and images.
         view (str): View of the image to use.
-        
+
     Returns:
         pd.DataFrame: DataFrame with the IDs and URLs.
     """
     df = df.reset_index()
     df = df.rename(columns={"index": "id"})
     df = df[["id", "images"]]
-    
+
     assert all(df["images"].apply(lambda x: isinstance(x, dict)))
     assert all(df["images"].apply(lambda x: view in x))
 
